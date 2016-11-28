057674f01b010e82d22580348704527ee6f3af9c
parkerbjones <parkerbjones@gmail.com>
2016-11-28 14:31:01 -0600

    Solution commit
    Programming Assignment 2 Solution

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmat <- function(matrix) m <<- matrix
+        getmat <- function() m
+        list(set = set, get = get,
+             setmat = setmat,
+             getmat = getmat)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmat()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(solve(m))
+        }
+        m <- x$get()
+        x$setmat(m)
+        solve(m)
 }
